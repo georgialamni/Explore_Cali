bb673f1a8f1d72afbab24955bc9119e8ea3519b0
Georgia <lgeorgia95@gmail.com>
2020-04-17 15:03:49 +0300

    Paging and Sorting

--- src/main/java/com/example/ec/explorecali/repo/TourRepository.java
+++ src/main/java/com/example/ec/explorecali/repo/TourRepository.java
@@ -1,10 +1,19 @@
 package com.example.ec.explorecali.repo;
 
 import com.example.ec.explorecali.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    /**
+     * Find Tours associated with the Tour Package
+     *
+     * @param code tour package code
+     * @return List of found tours
+     */
+    Page<Tour> findByTourPackageCode(String code, Pageable pageable);
 }
